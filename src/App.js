bd885be43bc5fcf463a72fab72c49107cf1efe09
Carlos Puente <carlospuenteg@gmail.com>
2022-04-02 13:31:10 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import './App.css';
 import Background from "./Background";
 
 export default function App() {
-    document.body.style.backgroundColor = "black";
     return (
     <div className="App">
         <Background />
